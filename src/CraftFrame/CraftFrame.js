e6f32dce286647b9e51f31a7212decb32eb22285
Oliver Oxenham <oliver@cardcity.co>
2018-11-05 23:21:13 +0400

    import components directly

--- src/CraftFrame/CraftFrame.js
+++ src/CraftFrame/CraftFrame.js
@@ -7,7 +7,7 @@ import '../../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css';
 import '../../node_modules/@blueprintjs/core/lib/css/blueprint.css';
 import './CraftFrame.scss';
 
-import { Craft } from '../components';
+import Craft from '../components/Craft/Craft';
 
 /* AppFrame */
 
