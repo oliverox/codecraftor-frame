eff2b3f6f8ed67ea0c2bfb21d8668751ada7422e
Oliver Oxenham <oliver@cardcity.co>
2020-01-31 12:57:51 +0800

    update

--- src/ComponentDrop/ComponentDrop.js
+++ src/ComponentDrop/ComponentDrop.js
@@ -1,70 +1,99 @@
-import React from 'react'
+import React from 'react';
 
-import styles from './ComponentDrop.module.css'
+import styles from './ComponentDrop.module.css';
 
 class ComponentDrop extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      isDragOver: false,
-    }
-    this.handleDrop = this.handleDrop.bind(this)
-    this.handleOnDragEnter = this.handleOnDragEnter.bind(this)
-    this.handleOnDragLeave = this.handleOnDragLeave.bind(this)
+      isDragOver: false
+    };
+    this.componentDropRef = React.createRef();
+    this.handleDrop = this.handleDrop.bind(this);
+    this.handleOnDragEnter = this.handleOnDragEnter.bind(this);
+    this.handleOnDragLeave = this.handleOnDragLeave.bind(this);
+  }
+
+  componentDidMount() {
+    
   }
 
   handleDrop(e) {
-    const { target, page } = this.props
-    const componentName = e.dataTransfer.getData('component')
-    const queryString = `componentName=${componentName}&target=${target}&page=${page}`
-    fetch(`/_cc/add?${queryString}`).then(() => {
-      this.setState({
-        isDragOver: false,
-      })
-    })
+    console.log('handleDrop()', e);
+    // const { target, page } = this.props
+    // const componentName = e.dataTransfer.getData('component')
+    // // const queryString = `componentName=${componentName}&target=${target}&page=${page}`
+    // // // fetch(`/_cc/add?${queryString}`).then(() => {
+    // // //   this.setState({
+    // // //     isDragOver: false,
+    // // //   })
+    // // // })
   }
 
   handleOnDragOver(e) {
-    e.stopPropagation()
-    e.preventDefault()
+    console.log('onDragOver()');
+    // e.stopPropagation()
+    e.preventDefault();
+    return false;
   }
 
   handleOnDragEnter() {
+    console.log('handleOnDragEnter()');
     this.setState({
-      isDragOver: true,
-    })
+      isDragOver: true
+    });
   }
 
   handleOnDragLeave() {
     this.setState({
-      isDragOver: false,
-    })
+      isDragOver: false
+    });
   }
 
   render() {
-    const { dropText } = this.props
-    let cn = styles.dropContainer
+    console.log('rendering ComponentDrop...');
+    const { dropText } = this.props;
+    let cn = styles.dropContainer;
     if (this.state.isDragOver) {
-      cn = `${cn} ${styles.isDragOver}`
+      cn = `${cn} ${styles.isDragOver}`;
     }
     return (
-      <div
-        className={cn}
-        onDrop={this.handleDrop}
-        onDragOver={this.handleOnDragOver}
-        onDragEnter={this.handleOnDragEnter}
-        onDragLeave={this.handleOnDragLeave}
-      >
-        {dropText}
+      <div>
+        {/* <div
+          onDragStart={() => {
+            console.log('onDragStart()');
+          }}
+          style={{width: 200, height: 100, backgroundColor: 'red'}}
+        >
+          
+        </div> */}
+        <div
+          ref={this.componentDropRef}
+          className={cn}
+          onDrop={() => {
+            console.log('onDrop');
+          }}
+          onDragOver={() => {
+            console.log('onDragOver');
+          }}
+          onDragEnter={() => {
+            console.log('onDragEnter');
+          }}
+          onDragLeave={() => {
+            console.log('onDragLeave');
+          }}
+        >
+          {dropText}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default ComponentDrop
+export default ComponentDrop;
 
 ComponentDrop.defaultProps = {
   target: 'root',
   page: 'index',
-  dropText: 'Drag & Drop Component Here',
-}
+  dropText: 'Drag & Drop Component Here'
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-// import { FocusStyleManager } from '@blueprintjs/core';
 import CraftFrame from './CraftFrame/CraftFrame';
 
-// FocusStyleManager.onlyShowFocusOnTabs();
 const rootEl = document.getElementById('root');
 
 ReactDOM.render(<CraftFrame />, rootEl);
